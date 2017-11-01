456e3fcf39d31f25817a16450f4e70e6efc53d83
HubertJiang <jiangxiankui@liweihui.com>
2017-11-01 16:29:27 +0800

    initialize

--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 import nav from './navigation';
 import categoryReducer from './categoryReducer';
+import mainReducer from './mainReducer';
 export default combineReducers({
     nav,
     categoryReducer,
+    mainReducer,
 });
